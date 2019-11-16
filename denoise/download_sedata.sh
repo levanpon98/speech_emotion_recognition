cba3f4e41ecaedd6aefa26a0918725b1e07bd48e
Le Van Pon <levanpon98@gmail.com>
2019-11-16 10:15:26 +0700

    update sh

--- denoise/download_sedata.sh
+++ denoise/download_sedata.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
 
-## DOWNLOAD THE DATASETS
 mkdir -p dataset_zip
 pushd dataset_zip
-# TRAINING DATASET
+
 if [ ! -f clean_trainset_28spk_wav.zip ]; then
     wget https://datashare.is.ed.ac.uk/bitstream/handle/10283/2791/clean_trainset_28spk_wav.zip
 fi
@@ -19,7 +17,6 @@ if [ ! -f noisy_testset_wav.zip ]; then
 fi
 popd
 
-## INFLATE DATA
 mkdir -p dataset_tmp
 pushd dataset_tmp
 unzip -q -j ../dataset_zip/clean_trainset_28spk_wav.zip -d trainset_clean
@@ -28,7 +25,6 @@ unzip -q -j ../dataset_zip/clean_testset_wav.zip -d valset_clean
 unzip -q -j ../dataset_zip/noisy_testset_wav.zip -d valset_noisy
 popd
 
-## RESAMPLE
 declare -a arr=("trainset_clean" "trainset_noisy" "valset_clean" "valset_noisy")
 
 mkdir -p dataset
@@ -43,5 +39,4 @@ for d in */; do
 done
 popd
 
-# REMOVE TMP DATA
 rm -r dataset_tmp
